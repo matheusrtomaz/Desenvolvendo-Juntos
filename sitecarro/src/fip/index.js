3bff775aa805df4a0f813f43b7b77a183dd32aa5
RICKMY GILIARDE DA SILVA <104576378+RicksilvaRS@users.noreply.github.com>
2024-04-25 14:09:23 -0300

    rixk

---
+++ sitecarro/src/fip/index.js
@@ -0,0 +1,19 @@
+import { Text,StyleSheet,View } from "react-native";
+
+export default function(){
+    return(
+        <View style={style.container}>
+            <Text>
+
+            </Text>
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    container:{
+        flex:1,
+
+    },
+
+});
